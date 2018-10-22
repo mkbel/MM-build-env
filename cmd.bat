f38ec700574faef91ee2e37274972a1e7913f24d
Marek Běl <marek.bel@posta.cz>
2018-10-22 20:47:11 +0200

    Add path to git and cat.

--- cmd.bat
+++
@@ -1,2 +0,0 @@
-SET PATH=%~dp0\cmake\bin;%~dp0\avr\bin;%~dp0\ninja
-start cmake-gui
---
+++ configure.bat
@@ -0,0 +1,2 @@
+SET PATH=%~dp0\cmake\bin;%~dp0\avr\bin;%~dp0\ninja;%~dp0\PortableGit\bin;%~dp0\PortableGit\usr\bin
+start cmake-gui
